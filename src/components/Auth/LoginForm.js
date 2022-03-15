009f7c848c2e22ea2ba5ad2e2bf67fa8085375a7
Tranquility Base <tfauvelle@gmail.com>
2022-03-14 20:28:04 -0400

    begin auth impl

---
+++ src/components/Auth/LoginForm.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const LoginForm = (props) => {
+
+    return (
+        <h1>Login Form</h1>
+    )
+}
+
+export default LoginForm;
---
+++ src/components/Auth/NewUserForm.js
@@ -0,0 +1,11 @@
+import { faHourglass1 } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+
+const NewUserForm = (props) => {
+
+    return (
+        <h1>Sign Up</h1>
+    )
+}
+
+export default NewUserForm;
